ce65610f7aee2b770b7c7d095013877da02aa5f9
Janak Khadka <janakkhadka08@gmail.com>
2025-02-27 21:38:58 +0545

    project created

---
+++ iOS Notification/iOS Notification/iOS_NotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_NotificationApp.swift
+//  iOS Notification
+//
+//  Created by Janak Khadka on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_NotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS Notification/iOS NotificationTests/iOS_NotificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_NotificationTests.swift
+//  iOS NotificationTests
+//
+//  Created by Janak Khadka on 27/02/2025.
+//
+
+import Testing
+@testable import iOS_Notification
+
+struct iOS_NotificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
