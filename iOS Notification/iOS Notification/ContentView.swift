a6a69b0a2ae516b81cf56a68873ec9ff689d4f59
Janak Khadka <janakkhadka08@gmail.com>
2025-02-27 22:55:49 +0545

    notification with asking for permission on appear

--- iOS Notification/iOS Notification/ContentView.swift
+++ iOS Notification/iOS Notification/ContentView.swift
@@ -12,16 +12,36 @@ struct ContentView: View {
         VStack {
             Button(
                 action: {
-                    
+                    scheduleNotification()
                 }
             ){
-                Text("Schedule Notification")
+                Text("Schedule")
+                    .padding()
                     .frame(width: 180, height: 40)
                     .background(Color.blue)
-                    .foregroundStyle(.white)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
             }
         }
         .padding()
+    }
+    func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "First Notification"
+        content.body = "my first notification hurray"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "test", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error aayo: \(error.localizedDescription)")
+            } else {
+                print("scheduled vayo")
+            }
+        }
     }
 }
 
--- iOS Notification/iOS Notification/iOS_NotificationApp.swift
+++ iOS Notification/iOS Notification/iOS_NotificationApp.swift
@@ -12,6 +12,24 @@ struct iOS_NotificationApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                                    requestNotificationPermission()
+                                }
         }
     }
+    
+    func requestNotificationPermission() {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+                if granted {
+                    print("Notification permission granted.")
+                } else if let error = error {
+                    print("Notification permission error: \(error.localizedDescription)")
+                }
+            }
+
+            // Check current notification settings
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                print("Notification settings: \(settings.authorizationStatus)")
+            }
+        }
 }
